bfe6751e3fd84eaeb46a44acdd01992d57f6b6fd
mcelhinneyk <mr.mcelhinney@gmail.com>
2022-04-03 19:49:58 -0700

    commit tree c1f947c37fb15c5719f880263a1e103ef1268ccc

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, length: {minimum:250}
+  validates :summary, length: {maximum:250}
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+  validate :click_bait
+
+  def click_bait
+    unless title.present? && title.include?("Won't Believe" || "Secret" || "Top [number]" || "Guess")
+      errors.add(:title, 'your title is not clickbait-y')
+    end
+  end
+
 end
